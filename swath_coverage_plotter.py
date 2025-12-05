1a980eef18be6684ad4fced4f98b9f50e83f4db4
Paul Johnson <pjohnson@ccom.nh>
2025-12-05 16:31:48 -0500

    Add BSD-3-Clause license header to swath_coverage_plotter.py

--- swath_coverage_plotter.py
+++ swath_coverage_plotter.py
@@ -4,6 +4,8 @@ Created on Fri Feb 15 14:33:30 2019
 
 @author: kjerram and pjohnson
 
+Center for Coastal and Ocean Mapping (CCOM)/ Joint Hydrographic Center (JHC), University of New Hampshire
+
 Multibeam Echosounder Assessment Toolkit: Swath Coverage Plotter
 
 This application provides a comprehensive GUI for analyzing and visualizing 
@@ -23,6 +25,36 @@ Key Features:
 - Multiple plot types and export options
 - Interactive data exploration tools
 """
+
+# BSD-3-Clause License
+#
+# Copyright (c) 2019, Center for Coastal and Ocean Mapping (CCOM)/ Joint Hydrographic Center (JHC), University of New Hampshire
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Standard library imports
 from PyQt6 import QtWidgets, QtGui
